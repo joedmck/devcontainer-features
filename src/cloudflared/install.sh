89e1d47ce31f110febe34daf529ec2bd9883fa15
Joe M <dev@joedm.com>
2023-04-22 09:54:16 +0000

    Remove duplicated package name

--- src/cloudflared/install.sh
+++ src/cloudflared/install.sh
@@ -5,7 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 mkdir -p --mode=0755 /usr/share/keyrings
 curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | tee /usr/share/keyrings/cloudflare-main.gpg > /dev/null
 source /etc/os-release
-echo "deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared ${VERSION_CODENAME} main" | sudo tee /etc/apt/sources.list.d/cloudflared.list
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared ${VERSION_CODENAME} main" | tee /etc/apt/sources.list.d/cloudflared.list
 
 apt-get update -yq
 apt-get -yq install --no-install-recommends cloudflared
